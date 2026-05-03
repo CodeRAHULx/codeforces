59b56b7ed897052818694bc3f7d9448b7d3e2aa7
CodeRAHULx <rahul565157@gmail.com>
2026-05-03 23:32:20 +0530

    Add Java solution for Codeforces 71A (Way Too Long Words)

---
+++ 71A/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String word = scanner.next();
+            if (word.length() > 10) {
+                System.out.println("" + word.charAt(0) + (word.length() - 2) + word.charAt(word.length() - 1));
+            } else {
+                System.out.println(word);
+            }
+        }
+    }
+}
